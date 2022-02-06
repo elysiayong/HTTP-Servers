c9f296ddad0d8ff47acaeeb6414d375d8279a8ac
sbhatoolaul <92624985+sbhatoolaul@users.noreply.github.com>
2022-02-05 21:50:14 -0500

    Add refactoring, malloc cleanup and connection type to code

--- A1/helpers/server.h
+++ A1/helpers/server.h
@@ -1,5 +1,4 @@
 #define MAXLINE 4096
-#define SA struct sockaddr
 
 int setup_server(int, char const *, int);
-void handle_client(int, int);
+int handle_client(int, int);
--- A1/helpers/utils.c
+++ A1/helpers/utils.c
@@ -98,14 +98,14 @@ time_t get_time(char* str, const char* format){
     return lt;
 }
 
-int get_port(int argc, char const *argv[]){
+int get_port(char const *arg){
     // Check if we got a valid port #
-    for (int i=0; i < strlen(argv[1]); i++){
-        if (!isdigit(argv[1][i])){
+    for (int i=0; i < strlen(arg); i++){
+        if (!isdigit(arg[i])){
             error_exit("port # is invalid...");
         }
     }
 
     // All good!
-    return atoi(argv[1]);
+    return atoi(arg);
 }
