f3b8cb0197f8c681fd7623f06199d2b49ef80bf8
elysiayong <elysiayong@gmail.com>
2022-02-09 20:44:26 -0500

    Add test results

--- A1/httperf_tests.sh
+++ A1/httperf_tests.sh
@@ -1,12 +1,12 @@
-# httperf --server localhost --port 18000 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 10 --num-conn 100 --num-call 10 --timeout 5 > httperf-logs/persistent/test1.out
-# httperf --server localhost --port 18000 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 10 --num-conn 100 --num-call 20 --timeout 5 > httperf-logs/persistent/test2.out
-# httperf --server localhost --port 18000 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 10 --num-conn 100 --num-call 30 --timeout 5 > httperf-logs/persistent/test3.out
+# httperf --server localhost --port 18000 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 10 --num-conn 100 --num-call 10 --timeout 5 > httperf-logs/persistent/test1.txt
+# httperf --server localhost --port 18000 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 10 --num-conn 100 --num-call 20 --timeout 5 > httperf-logs/persistent/test2.txt
+# httperf --server localhost --port 18000 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 10 --num-conn 100 --num-call 30 --timeout 5 > httperf-logs/persistent/test3.txt
 
-# httperf --server localhost --port 18000 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 10 --num-conn 100 --num-call 10 --timeout 5 > httperf-logs/pipelined/test1.out
-# httperf --server localhost --port 18000 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 10 --num-conn 100 --num-call 20 --timeout 5 > httperf-logs/pipelined/test2.out
-# httperf --server localhost --port 18000 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 10 --num-conn 100 --num-call 30 --timeout 5 > httperf-logs/pipelined/test3.out
+# httperf --server localhost --port 18000 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 10 --num-conn 100 --num-call 10 --timeout 5 > httperf-logs/pipelined/test1.txt
+# httperf --server localhost --port 18000 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 10 --num-conn 100 --num-call 20 --timeout 5 > httperf-logs/pipelined/test2.txt
+# httperf --server localhost --port 18000 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 10 --num-conn 100 --num-call 30 --timeout 5 > httperf-logs/pipelined/test3.txt
 
-# httperf --server 192.168.0.32 --port 80 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 10 --num-conn 100 --num-call 10 --timeout 5 > httperf-logs/apache/test1.out
-# httperf --server 192.168.0.32 --port 80 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 10 --num-conn 100 --num-call 20 --timeout 5 > httperf-logs/apache/test2.out
-# httperf --server 192.168.0.32 --port 80 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 10 --num-conn 100 --num-call 30 --timeout 5 > httperf-logs/apache/test3.out
+# httperf --server 192.168.0.32 --port 80 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 10 --num-conn 100 --num-call 10 --timeout 5 > httperf-logs/apache/test1.txt
+# httperf --server 192.168.0.32 --port 80 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 10 --num-conn 100 --num-call 20 --timeout 5 > httperf-logs/apache/test2.txt
+# httperf --server 192.168.0.32 --port 80 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 10 --num-conn 100 --num-call 30 --timeout 5 > httperf-logs/apache/test3.txt
 
