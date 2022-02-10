23c09f87fd3906a839c7113d22d1469833595c00
elysiayong <elysiayong@gmail.com>
2022-02-10 14:34:08 -0500

    Add simple test

--- A1/httperf_tests.sh
+++ A1/httperf_tests.sh
@@ -1,3 +1,8 @@
+# httperf --server localhost --port 18000 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 1000 --num-conn 1000 --num-call 1 > httperf-logs/simple/test1.txt
+# httperf --server localhost --port 18000 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 2000 --num-conn 2000 --num-call 1 > httperf-logs/simple/test2.txt
+# httperf --server localhost --port 18000 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 3000 --num-conn 3000 --num-call 1 > httperf-logs/simple/test3.txt
+
+
 # httperf --server localhost --port 18000 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 10 --num-conn 100 --num-call 10 --timeout 5 > httperf-logs/persistent/test1.txt
 # httperf --server localhost --port 18000 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 10 --num-conn 100 --num-call 20 --timeout 5 > httperf-logs/persistent/test2.txt
 # httperf --server localhost --port 18000 --uri /www.cs.toronto.edu/~ylzhang/csc258/memes.html --rate 10 --num-conn 100 --num-call 30 --timeout 5 > httperf-logs/persistent/test3.txt
